2a9d6429a466412f6b97e2e1a5858ace921ccfcd
Ruben Cuevas Fernandez <Ruben.Cuevas@unit4.com>
2026-05-22 13:59:02 +0200

    style(compass): thicker rings, inner shadow depth

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker básico para PWA: cache-first del shell, network-first de datos.
-const CACHE = "viento-cenes-v23";
+const CACHE = "viento-cenes-v24";
 const SHELL = [
   "./",
   "./index.html",
