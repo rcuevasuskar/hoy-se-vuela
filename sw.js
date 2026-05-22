8d37f59954689f9638a0d9080298bd9c738b7b13
Ruben Cuevas Fernandez <Ruben.Cuevas@unit4.com>
2026-05-22 13:55:33 +0200

    style(compass): match red, shrink outer ring, sharper longer wedge, cardinals outside

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker básico para PWA: cache-first del shell, network-first de datos.
-const CACHE = "viento-cenes-v21";
+const CACHE = "viento-cenes-v22";
 const SHELL = [
   "./",
   "./index.html",
