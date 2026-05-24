c5f2392ba1217776d5d7b3fe7f4170b142287ce6
Ruben Cuevas Fernandez <Ruben.Cuevas@unit4.com>
2026-05-24 16:45:13 +0200

    v89: revierte filtro logueado; muestra todas las estaciones

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker básico para PWA: cache-first del shell, network-first de datos.
-const CACHE = "viento-cenes-v88";
+const CACHE = "viento-cenes-v89";
 const SHELL = [
   "./",
   "./index.html",
