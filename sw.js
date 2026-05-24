2eed3b6c4bb8f0deed4f7826ae4f77b6fb964bc3
Ruben Cuevas Fernandez <Ruben.Cuevas@unit4.com>
2026-05-24 15:36:25 +0200

    v74: separador de dia en pronostico mas visible (linea naranja gruesa + pill con dia)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker básico para PWA: cache-first del shell, network-first de datos.
-const CACHE = "viento-cenes-v73";
+const CACHE = "viento-cenes-v74";
 const SHELL = [
   "./",
   "./index.html",
